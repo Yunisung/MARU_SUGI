a8b942dd529573b9a77566d20b281f0d0a60ec24
pys890102 <pys890102@bkwinnners.com>
2022-02-15 17:59:06 +0900

    프로세스 이름 변경

--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,7 @@
 
 # JVM_ARGS for VM
 ##########################
-JVM_ARGS="-DMARU_WALLET_APP -server -DCP_CONF=../conf -Dlogback.configurationFile=../conf/logback.xml -Dfile.encoding=UTF-8 -Djava.io.tmpdir=../web/upload"
+JVM_ARGS="-DMARU_SUGI -server -DCP_CONF=../conf -Dlogback.configurationFile=../conf/logback.xml -Dfile.encoding=UTF-8 -Djava.io.tmpdir=../web/upload"
 JVM_ARGS="$JVM_ARGS -Xss512k -Xms1024m -Xmx2048m"
 JVM_ARGS="$JVM_ARGS -cp ../lib/*:../lib/apache/*:../lib/tomcat/*:../lib/spring/*:../web/WEB-INF/classes"
 
