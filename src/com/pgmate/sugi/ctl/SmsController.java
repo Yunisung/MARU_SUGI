2786b2a0b4a0e75128ac40c2edee3e226f509bf2
boram0415 <boram04415@naver.com>
2022-05-03 17:24:59 +0900

    sms > baseURL 변경

--- src/com/pgmate/sugi/ctl/SmsController.java
+++ src/com/pgmate/sugi/ctl/SmsController.java
@@ -62,7 +62,7 @@ import com.pgmate.lib.util.map.SharedMap;
 public class SmsController {
 	
 	private static Logger logger = LoggerFactory.getLogger( com.pgmate.sugi.ctl.SmsController.class );
-	private static final String BASE_URL = "https://api.bkwinners.kr";
+	private static final String BASE_URL = "10.100.100.10:10002";
 	
 	@RequestMapping(value="/sms/{smsKey}/pay")
 	public ModelAndView smsPay(HttpServletRequest request ,@PathVariable("smsKey") String smsKey) {
