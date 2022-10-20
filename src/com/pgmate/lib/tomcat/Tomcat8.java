f5d2cd2fb6b2b9bfa96d447a5d7bab19d5b82836
yangwenry <yangwenry@bkwinners.com>
2022-10-20 15:35:07 +0900

    젠킨스 파일생성 초기화

--- src/com/pgmate/lib/tomcat/Tomcat8.java
+++ src/com/pgmate/lib/tomcat/Tomcat8.java
@@ -17,9 +17,6 @@ import com.pgmate.lib.conf.ConfigLoader;
 import com.pgmate.lib.tomcat.conf.TomcatConfigBean;
 import com.pgmate.lib.util.lang.CommonUtil;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-
 
 /**
  * @author Administrator
@@ -150,44 +147,12 @@ public class Tomcat8 {
 	}
 	
 	public static void main(String[] args){
-		/*
 		Tomcat8 tomcat = new Tomcat8();
 		try{
 			tomcat.start();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		*/
-		createFile();
 	}
-	
-	private static void createFile() {
-		
-		String txt = "테스트입니다!!" ;
-		
-		String fileName = "/home/bkwinners/MARU_SUGI/bin/test11.txt" ;
-		
-		
-		try{
-			
-			// 파일 객체 생성
-			File file = new File(fileName) ;
-			
-			// true 지정시 파일의 기존 내용에 이어서 작성
-			FileWriter fw = new FileWriter(file, true) ;
-			
-			// 파일안에 문자열 쓰기
-			fw.write(txt);
-			fw.flush();
-
-			// 객체 닫기
-			fw.close(); 
-			
-			
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-
 
 }
