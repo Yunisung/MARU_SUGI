f73063eab73927f60a8df4b5259e3cbc89d2ec14
yangwenry <yangwenry@bkwinners.com>
2022-10-21 13:07:31 +0900

    stop 프로세츠 체크

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,3 +1,11 @@
 #!/bin/sh 
-kill -9 `cat < pwd.pid`
-rm pwd.pid
+#kill -9 `cat < pwd.pid`
+PID=`cat < pwd.pid`
+
+if [ -z $PID ]; then
+    echo 'PID가 존재하지 않습니다.'
+else 
+	echo 'PID ========> ${PID}'
+fi
+
+#rm pwd.pid
