8d065f64f87ed5116d2848ef65ad1059950d3014
yangwenry <yangwenry@bkwinners.com>
2022-10-21 10:49:33 +0900

    젠킨스용 시작

---
+++ bin/start_jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd /home/bkwinners/MARU_SUGI/bin
+
+export JAVA_PATH=/usr/local/jdk1.8.0_271/bin
+export MARU_PATH=/home/bkwinners/MARU_SUGI
+
+# JVM_ARGS for VM
+##########################
+JVM_ARGS="-DMARU_SUGI -server -DCP_CONF=$MARU_PATH/conf -Dlogback.configurationFile=$MARU_PATH/conf/logback.xml -Dfile.encoding=UTF-8 -Djava.io.tmpdir=$MARU_PATH/web/upload"
+JVM_ARGS="$JVM_ARGS -Xss512k -Xms1024m -Xmx2048m"
+JVM_ARGS="$JVM_ARGS -cp $MARU_PATH/lib/*:$MARU_PATH/lib/apache/*:$MARU_PATH/lib/tomcat/*:$MARU_PATH/lib/spring/*:$MARU_PATH/web/WEB-INF/classes"
+
+nohup $JAVA_PATH/java $JVM_ARGS com.pgmate.lib.tomcat.Tomcat8 > /dev/null 2>&1 &
+echo $!>pwd.pid
