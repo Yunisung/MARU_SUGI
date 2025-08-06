91e7fa6a8553b55bd35023813147f45b4fdf7e65
pys890102 <pys890102@bkwinners.com>
2025-08-06 11:01:24 +0900

    250806_결제 주소 롤백

--- src/com/pgmate/sugi/ctl/SmsController.java
+++ src/com/pgmate/sugi/ctl/SmsController.java
@@ -47,14 +47,14 @@ import com.pgmate.lib.util.map.SharedMap;
 public class SmsController {
 	
 	private static Logger logger = LoggerFactory.getLogger( com.pgmate.sugi.ctl.SmsController.class );
-	private static final String BASE_URL = "https://api.ghpayments.kr";
+	private static final String BASE_URL = "http://10.100.100.12:10002";
 	
 	@RequestMapping(value="/sms/{smsKey}/pay")
 	public ModelAndView smsPay(HttpServletRequest request ,@PathVariable("smsKey") String smsKey) {
 		SmsDAO smsDAO = new SmsDAO();
 		
 		SharedMap<String, Object> smsPay = smsDAO.getSmsPay(smsKey);
-		request.setAttribute("baseUrl", BASE_URL);
+		request.setAttribute("baseUrl", "https://api.ghpayments.co.kr");
 		if(smsPay != null){
 			if("N".equals(smsPay.getString("status"))){
 				smsPay.put("payerTel", smsDAO.getAESDec(smsPay.getString("payerTel")));
